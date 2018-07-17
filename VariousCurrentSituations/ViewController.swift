b1500cf6bd8222feb792b9ee4745252b7ffd8e85
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-07-18 01:07:08 +0900

    initial commit

---
+++ VariousCurrentSituations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VariousCurrentSituations
+//
+//  Created by 伊賀裕展 on 2018/07/18.
+//  Copyright © 2018年 伊賀裕展. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
