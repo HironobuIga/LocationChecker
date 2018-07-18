db84de3161a61ec9a131b98b275b0a9a46d1a7c6
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-07-19 01:32:53 +0900

    [update]位置情報を取得し、地名を表示する機能を追加

--- VariousCurrentSituations/ViewController.swift
+++ VariousCurrentSituations/ViewController.swift
@@ -10,18 +10,39 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet private weak var locationMameLabel: UILabel!
+    
+    @IBOutlet weak var updateButton: UIButton! {
+        didSet {
+            updateButton.clipsToBounds = true
+            updateButton.layer.cornerRadius = 4.0
+        }
+    }
+    @IBAction func updateButtonTapped(_ sender: UIButton) {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .denied, .notDetermined, .restricted:
+            return
+        }
+    }
     
     // MARK: - Property
     var locationManager: CLLocationManager!
-
+    var locationName: String? {
+        didSet {
+            // Labelの更新
+            locationMameLabel.text = locationName
+        }
+    }
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         locationManager = CLLocationManager()
         locationManager.delegate = self
-        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
     }
 
     // MARK: - Method
@@ -30,7 +51,24 @@ class ViewController: UIViewController {
 
 // MARK: - Private Method
 private extension ViewController {
-    
+    private func getLocationName(location: CLLocation, completionHandler: @escaping (String?) -> Void) {
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(location) { (landmarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            let string = landmarks?.first?.addressDictionary?.compactMap({ (dic) -> String? in
+                if let string = dic.value as? String {
+                    return string
+                } else {
+                    return nil
+                }
+            }).joined(separator: ",")
+            return completionHandler(string)
+        }
+    }
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -45,6 +83,14 @@ extension ViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
+        guard let location = locations.first else { return }
+        getLocationName(location: location) { [weak self] locationName in
+            guard let `self` = self else { return }
+            self.locationName = locationName
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
