7675a438ad269a724d0df1ff3f20063f615bbfd6
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-07-23 06:20:55 +0900

    LocationManagerの処理をViewControllerではなく、ViewModelで処理するよう変更

--- VariousCurrentSituations/Screen/CurrentLocationViewController.swift
+++ VariousCurrentSituations/Screen/CurrentLocationViewController.swift
@@ -21,16 +21,13 @@ final class CurrentLocationViewController: UIViewController {
     
     @IBAction private func updateButtonTapped(_ sender: UIButton) {
         switch CLLocationManager.authorizationStatus() {
-        case .authorizedAlways, .authorizedWhenInUse:
-            locationManager.requestLocation()
-        case .denied, .notDetermined, .restricted:
-            return
+        case .authorizedAlways, .authorizedWhenInUse: viewModel.fetchLocationAndLocationName()
+        case .denied, .notDetermined, .restricted: return
         }
     }
     
     // MARK: - Property
-    let viewModel = ViewModel()
-    var locationManager: CLLocationManager!
+    let viewModel = CurrentLocationViewModel()
     var locationName: String? {
         didSet {
             locationMameLabel.text = locationName // Labelの更新
@@ -40,9 +37,6 @@ final class CurrentLocationViewController: UIViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager = CLLocationManager()
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.delegate = self
         
         viewModel.didChangeState = { [weak self] state in
             guard let `self` = self else { return }
@@ -62,29 +56,5 @@ final class CurrentLocationViewController: UIViewController {
                 self.present(alertController, animated: true, completion: nil)
             }
         }
-    }
-}
-
-// MARK: - Private Method
-private extension CurrentLocationViewController {
-}
-
-// MARK: - CLLocationManagerDelegate
-extension CurrentLocationViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        switch status {
-        case .authorizedAlways, .authorizedWhenInUse: break // 許可済み
-        case .denied, .restricted: break // 許可していない
-        case .notDetermined: locationManager.requestWhenInUseAuthorization()
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first else { return }
-        viewModel.fetchLocationName(location)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error.localizedDescription)
     }
 }
---
+++ VariousCurrentSituations/Screen/CurrentLocationViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  CurrentLocationViewModel.swift
+//  VariousCurrentSituations
+//
+//  Created by iganin on 2018/07/21.
+//  Copyright © 2018年 iganin. All rights reserved.
+//
+
+import CoreLocation
+
+final class CurrentLocationViewModel: NSObject {
+    enum State {
+        case none, loading, success(string: String), failure(error: Error?)
+    }
+    
+    // MARK: - Property
+    private var state: State = .none {
+        didSet {
+            didChangeState?(state)
+        }
+    }
+    private let geoCoderRequester = GeoCoderRequester()
+    private let locationManager = CLLocationManager()
+    var didChangeState: ((State) -> Void)?
+    
+    // MARK: - Life Cycle
+    override init() {
+        super.init()
+        geoCoderRequester.didChangeState = { [weak self] state in
+            guard let `self` = self else { return }
+            switch state {
+            case .none, .loading: break
+            case .failure(let error): self.state = .failure(error: error)
+            case .success(let result): self.state = .success(string: result)
+            }
+        }
+        locationManager.delegate = self
+    }
+    
+    // MARK: - Function
+    func fetchLocationAndLocationName() {
+        state = .loading
+        locationManager.requestLocation()
+    }
+    
+}
+
+private extension CurrentLocationViewModel {
+    private func fetchLocationName(_ location: CLLocation?) {
+        guard let location = location else { return }
+        geoCoderRequester.getLocationInformations(location: location)
+    }
+}
+
+extension CurrentLocationViewModel: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse: break // 許可済み
+        case .denied, .restricted: break // 許可していない
+        case .notDetermined: locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        fetchLocationName(location)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        state = .failure(error: error)
+        print(error.localizedDescription)
+    }
+}
